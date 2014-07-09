a8eb26a11d5daf8090bc3020b32f6938c068afcf
Shreyas <skraman1999@gmail.com>
2014-07-09 11:43:25 -0700

    First commit. It works, but input is not properly done yet.

---
+++ src/game/Game.java
@@ -0,0 +1,92 @@
+package game;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+
+public class Game extends JFrame implements MouseListener, KeyListener, Runnable {
+    
+    public static final int SCREEN_WIDTH = 640;
+    public static final int SCREEN_HEIGHT = 480;
+    public static final BufferedImage offscreen = new BufferedImage(SCREEN_WIDTH ,SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+    
+    int x;
+    int y;
+    
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+
+    public Game() {
+        x = 0;
+        y = 0;
+        setVisible(true);
+        setSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setTitle("Game");
+        addKeyListener(this);
+        addMouseListener(this);
+    }
+    
+    public void start() {
+        new Thread(this).start();
+    }
+    
+    @Override
+    public void run() {
+        while(true) {
+            update();
+            try {
+                Thread.sleep(1000/60);
+            } catch (InterruptedException e) {
+                System.out.println("o.0 " + e);
+            }
+        }
+    }
+    
+    public void update() {
+        x++;
+        y++;
+    }
+    
+    @Override
+    public void paint(Graphics g) {
+        Graphics s = offscreen.getGraphics();
+        
+        s.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        
+        s.setColor(Color.red);
+        s.fillRect(x, y, 32, 32);
+        
+        g.drawImage(offscreen, 0, 0, this);
+        
+        repaint();
+    }
+    
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+        System.out.println("T: " + e.getKeyChar());
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("P: " + e.getKeyCode());
+    }
+
+    @Override public void keyReleased(KeyEvent e) { }
+    
+    @Override public void mouseClicked(MouseEvent e) { }
+    @Override public void mousePressed(MouseEvent e) { }
+    @Override public void mouseReleased(MouseEvent e) { }
+    @Override public void mouseEntered(MouseEvent e) { }
+    @Override public void mouseExited(MouseEvent e) { }
+}
